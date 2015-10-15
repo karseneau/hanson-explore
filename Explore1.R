8a91e377e86e43919d5b285ada06b40b1b56e68e
unknown <arseneauk@AUSCL4MIL004.grouphc.net>
2015-10-15 10:55:14 +1000

    Have added mean to boxplox

--- Explore1.R
+++ Explore1.R
@@ -6,4 +6,6 @@ ii <- data.frame(matrix(aperm(iris3, c(1,3,2)), ncol = 4,
                  Species = gl(3, 50, labels = sub("S", "s", sub("V", "v", dni3[[3]]))))
 all.equal(ii, iris) # TRUE
 
-boxplot(Petal.Length~Species, data=ii) 
+boxplot(Petal.Length~Species, data=ii)
+means <- tapply(ii$Petal.Length,ii$Species,mean)
+points(means,col="red",pch=18)
